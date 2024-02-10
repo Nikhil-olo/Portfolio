ebbc2860ab1356e543d3d5faa8cf3e54a7387535
NIkhil Verma <83595187+Nikhil-olo@users.noreply.github.com>
2024-02-10 20:26:36 +0530

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,8 +5,8 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  // const computer = useGLTF("./desktop_pc/scene.gltf");
-  const computer = useGLTF("./public/desktop_pc/scene.gltf");
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+  // const computer = useGLTF("./public/desktop_pc/scene.gltf");
 
   return (
     <mesh>
